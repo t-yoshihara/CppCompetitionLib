d9c8eedc011cca0b4db73d03f514d28a3f036c50
t-yoshihara <inmir_331@outlook.jp>
2017-09-23 19:40:01 +0900

    Update templete.cpp

--- templete.cpp
+++ templete.cpp
@@ -2,6 +2,8 @@
 #define REP(i,n) for(int i=0;i<(int)(n);i++)
 #define ALL(x) (x).begin(),(x).end()
 #define LL long long
+#define pii pair<int,int>
+#define pll pair<LL,LL>
 
 using namespace std;
 
