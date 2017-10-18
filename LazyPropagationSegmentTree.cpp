5c936d9fa676e976d5fb8a6af3314a6a9a1dcf35
吉原興宙 <yoshiwaratomohiro@Yoshihara-MacBook-Pro.local>
2017-10-18 19:06:00 +0900

    SegmentTree

---
+++ LazyPropagationSegmentTree.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define REP(i,n) for(int i=0;i<(int)(n);i++)
+#define ALL(x) (x).begin(),(x).end()
+#define LL long long
+
+using namespace std;
+
+//BEGIN CUT HERE
+
+struct SegmentTree{
+
+    vector<int> data;
+    vector<int> lazy;
+    int n;
+
+    SegmentTree(int n_){
+        n=1;
+        while(n<n_)n*=2;
+        data.resize(n*2-1);
+        lazy.resize(n*2-1);
+    }
+
+    void eval(int k,int a,int b){
+        data[k]+=lazy[k];
+        if(k<n-1){
+            lazy[2*k+1]+=lazy[k]/2;///合計なので
+            lazy[2*k+2]+=lazy[k]/2;///半分にして追加している
+        }
+        lazy[k]=0;
+    }
+    
+    void add(int a,int b,int x,int k,int l,int r){
+        eval(k,l,r);
+
+        if(l>=b||r<=a)return;
+        if(l>=a&&r<=b){
+            lazy[k]+=(r-l)*x;///合計なのでr-lを掛けている
+            eval(k,l,r);
+            return;
+        }
+        add(a,b,x,2*k+1,l,(l+r)/2);
+        add(a,b,x,2*k+2,(l+r)/2,r);
+        data[k]=(data[2*k+1]+data[2*k+2]);///合計なので子ノードの和
+    }
+
+    //[a,b)
+    void add(int a,int b,int x){
+        add(a,b,x,0,0,n);
+    }
+
+    int getSum(int a,int b,int k,int l,int r){
+        eval(k,l,r);
+
+        if(l>=b||r<=a)return 0;
+        if(l>=a&&r<=b)return data[k];
+
+        return (getSum(a,b,2*k+1,l,(r+l)/2) + getSum(a,b,2*k+2,(r+l)/2,r));///合計なので子ノードの和
+    }
+
+    //[a,b)
+    int getSum(int a,int b){
+        return getSum(a,b,0,0,n);
+    }
+
+};
+
+//END CUT HERE
+
+int main(){
+
+
+
+    return 0;
+}
--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,56 +1,106 @@
+#include <bits/stdc++.h>
+#define REP(i,n) for(int i=0;i<(int)(n);i++)
+#define ALL(x) (x).begin(),(x).end()
+#define LL long long
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+
+using namespace std;
+
+//BEGIN CUT HERE
+
+///点加算区間総和（＝ BIT )
 struct SegmentTree{
 
-    vector<int> data;
-    vector<int> lazy;
     int n;
+    vector<int> data;
 
     SegmentTree(int n_){
         n=1;
         while(n<n_)n*=2;
         data.resize(n*2-1);
-        lazy.resize(n*2-1);
     }
 
-    void eval(int k,int a,int b){
-        data[k]+=lazy[k];
-        if(k<n-1){
-            lazy[2*k+1]+=lazy[k]/2;///合計なので
-            lazy[2*k+2]+=lazy[k]/2;///半分にして追加している
+    void add(int vol,int plc){
+        plc += n-1;
+        data[plc]+=vol;
+        while(plc){
+            plc=(plc-1)/2;
+            data[plc] = data[plc*2+1]+data[plc*2+2];
         }
-        lazy[k]=0;
-    }
-    
-    void add(int a,int b,int x,int k,int l,int r){
-        eval(k,l,r);
-
-        if(l>=b||r<=a)return;
-        if(l>=a&&r<=b){
-            lazy[k]+=(r-l)*x;///合計なのでr-lを掛けている
-            eval(k,l,r);
-            return;
-        }
-        add(a,b,x,2*k+1,l,(l+r)/2);
-        add(a,b,x,2*k+2,(l+r)/2,r);
-        data[k]=(data[2*k+1]+data[2*k+2]);///合計なので子ノードの和
     }
 
-    //[a,b)
-    void add(int a,int b,int x){
-        add(a,b,x,0,0,n);
-    }
-
-    int getSum(int a,int b,int k,int l,int r){
-        eval(k,l,r);
-
-        if(l>=b||r<=a)return 0;
-        if(l>=a&&r<=b)return data[k];
-
-        return (getSum(a,b,2*k+1,l,(r+l)/2) + getSum(a,b,2*k+2,(r+l)/2,r));///合計なので子ノードの和
-    }
-
-    //[a,b)
-    int getSum(int a,int b){
-        return getSum(a,b,0,0,n);
+    int getSum(int a,int b,int k=0,int l=0,int r=n){
+        if(r<=a||l>=b)return 0;
+        if(r>=a&&l<=b)return data[k];
+        return getSum(a,b,2*k+1,l,(l+r)/2) + getSum(a,b,2*k+2,(l+r)/2,r);
     }
 
 };
+
+///点代入区間最大(最小)
+struct SegmentTree{
+
+    int n;
+    vector<int> data;
+
+    SegmentTree(int n_){
+        n=1;
+        while(n<n_)n*=2;
+        data.resize(n*2-1);
+    }
+
+    void init(int vol,int plc){
+        plc += n-1;
+        data[plc]=vol;
+        while(plc){
+            plc=(plc-1)/2;
+            data[plc] = max(data[plc*2+1] ,data[plc*2+2]);
+        }
+    }
+
+    int getMax(int a,int b,int k=0,int l=0,int r=n){
+        if(r<=a||l>=b)return INT_MIN;
+        if(r>=a&&l<=b)return data[k];
+        return max(getMax(a,b,2*k+1,l,(l+r)/2), getSum(a,b,2*k+2,(l+r)/2,r));
+    }
+};
+
+
+///区間加算区間最大(最小) (StarrySky木)
+struct SegmentTree{
+
+    int n;
+    vector<int> segAdd;
+    vector<int> segDat;
+
+    SegmentTree(int n_){
+        n=1;
+        while(n<n_)n*=2;
+        segAdd.resize(n*2-1);
+        segDat.resize(n*2-1);
+    }
+
+    void add(int a,int b,int p,int k=0,int l=0,int r=n){
+        if(r<=a||l>=b)return;
+        if(r>=a&&l<=b){
+            segAdd[k]+=p;
+            return;
+        }
+        add(a,b,p,2*k+1,l,(l+r)/2);
+        add(a,b,p,2*k+2,(l+r)/2,r);
+        segDat[k] = max(segDat[2*k+1]+segAdd[2*k+1],segDat[2*k+2]+segAdd[2*k+2]);
+    }
+
+    int getMax(int a,int b,int k=0,int l=0,int r=n){
+        if(r<=a||l>=b)return INT_MIN;
+        if(r>=a&&l<=b)return segDat[k]+segAdd[k];
+        return max(getMax(a,b,2*k+1,l,(l+r)/2), getSum(a,b,2*k+2,(l+r)/2,r)) + segAdd[k];
+    }
+};
+
+
+int main(){
+
+    return 0;
+}
